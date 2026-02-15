0996eddc96a61ec9dd7135fb60e1ef94e4b46e47
Brent Arata <brent.arata@gmail.com>
2026-02-15 02:22:01 -0800

    ci: retain playwright failures, change aspect ratio of github playwright runner, upload image as artifact

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,9 +6,11 @@ export default defineConfig({
   reportSlowTests: { max: 1, threshold: 60_000 },
   use: {
     headless: false,               // IMPORTANT for CAPTCHA/MFA
+    locale: 'en-US',
+    timezoneId: 'America/Los_Angeles', 
     viewport: { width: 1280, height: 800 },
-    screenshot: 'off', 
-    video: 'off',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     actionTimeout: 15_000,
      // 1. Toggles bypassing Content-Security-Policy globally
     bypassCSP: true, 
